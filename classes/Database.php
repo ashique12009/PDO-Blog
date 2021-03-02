bffdc71b120fced8dd4885b560ae4d96805fb9a3
Ashique <ashique12009@gmail.com>
2021-03-02 22:11:17 +0600

    Fetching data

--- classes/Database.php
+++ classes/Database.php
@@ -2,7 +2,7 @@
 /**
  * Database class file
  */
-class ClassName
+class Database
 {
   private $host = "localhost";
   private $user = "root";
@@ -26,9 +26,46 @@ class ClassName
 
     // Create new PDO instance
     try {
-      $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+      $this->dbh = new PDO($dsn, $this->user, $this->password, $options);
     } catch (PDOException $e) {
       $this->error = $e->getMessage();
     }
+  }
+
+  public function query($query)
+  {
+    $this->stmt = $this->dbh->prepare($query);
+  }
+
+  public function bind($param, $value, $type = null)
+  {
+    if (is_null($type)) {
+      switch (true) {
+        case is_int($value):
+          $type = PDO::PARAM_INT;
+          break;
+        case is_bool($value):
+          $type = PDO::PARAM_BOOL;
+          break;
+        case is_null($value):
+          $type = PDO::PARAM_NULL;
+          break;
+        default:
+          $type = PDO::PARAM_STR;
+          break;
+      }
+    }
+    $this->stmt->bindValue($param, $value, $type);
+  }
+
+  public function execute()
+  {
+    return $this->stmt->execute();
+  }
+
+  public function resultset()
+  {
+    $this->execute();
+    return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
   }
 }
--- index.php
+++ index.php
@@ -1,1 +1,9 @@
 <?php 
+require 'classes/Database.php';
+
+$database = new Database;
+
+$database->query('SELECT * FROM posts');
+$rows = $database->resultset();
+
+var_dump($rows);
