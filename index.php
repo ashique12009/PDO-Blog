983cc48220a619f636a8ff787520fa4f92c7cb59
Ashique <ashique12009@gmail.com>
2021-03-03 22:03:20 +0600

    Fetching data and showing in table

--- index.php
+++ index.php
@@ -5,5 +5,23 @@ $database = new Database;
 
 $database->query('SELECT * FROM posts');
 $rows = $database->resultset();
+?>
 
-var_dump($rows);
+<?php if(count($rows)> 0) :?>
+  <table>
+    <tr>
+      <td>Title</td>
+      <td>Body</td>
+      <td>Created Date</td>
+    </tr>
+    <?php foreach ($rows as $value) : ?>
+      <tr>
+        <td><?php echo $value['title']; ?></td>
+        <td><?php echo $value['body']; ?></td>
+        <td><?php echo $value['create_date']; ?></td>
+      </tr>  
+    <?php endforeach; ?>
+  </table>
+<?php else : ?>
+  <h5>No record found!</h5>
+<?php endif; ?>
